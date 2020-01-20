8707d1a72b2a9e87cc8f0b5061749c8dcfca959a
Sabrina <delrock876@gmail.com>
2020-01-20 15:50:18 -0800

    changed form, added datepicker

--- client/src/components/JobForm/JobForm.js
+++ client/src/components/JobForm/JobForm.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
-// import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
 
 
@@ -16,37 +15,56 @@ const useStyles = makeStyles(theme => ({
   },
 }))
 
-const JobForm =()=> {
+const JobForm = () => {
+
   const classes = useStyles()
 
   return (
 
-  <Grid container justify="flex-end">
+    <Grid container justify="flex-end">
 
-    <form className={classes.root} noValidate autoComplete="off">
-  
-      <TextField size="small" id="outlined-basic" label="Company Name" variant="outlined" />
-      <TextField size="small" id="outlined-basic" label="Date Applied" variant="outlined" />
-      <br/>
-      <TextField size="small" id="outlined-basic" label="Skills/Requirements" variant="outlined" />
-      <TextField size="small" id="outlined-basic" label="Connections" variant="outlined" />
-      <br/>
-      <TextField size="small" id="outlined-basic" label="Next Step" variant="outlined" />
-      <TextField size="small" id="outlined-basic" label="Progress" variant="outlined" />
-      <br/>
-      <TextField
-        id="filled-multiline-static"
-        label="Notes"
-        multiline
-        rows="4"
-        // defaultValue="Default Value"
-        variant="filled"
-      />
-      <br/>
-      <Button>Submit</Button>
-    </form>
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField size="small" id="outlined-basic" label="Company Name" variant="outlined" />
+
+        <TextField
+          size="small"
+          id="date"
+          label="Date Applied"
+          type="date"
+          variant= "outlined"
+          className={classes.textField}
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+        <br />
+        <TextField size="small" id="outlined-basic" label="Connection Type" variant="outlined" />
+        <TextField size="small" id="outlined-basic" label="Connection Name" variant="outlined" />
+        <br />
+        <TextField size="small" id="outlined-basic" label="Current Status" variant="outlined" />
+        <TextField size="small" id="outlined-basic" label="Next Step" variant="outlined" />
+        <br />
+        <TextField
+          id="filled-multiline-static"
+          label="Notes"
+          multiline
+          rows="4"
+          // defaultValue="Default Value"
+          variant="filled"
+        />
+        <TextField
+          id="filled-multiline-static"
+          label="Skill Requirements"
+          multiline
+          rows="4"
+          // defaultValue="Default Value"
+          variant="filled"
+        />
+        <br />
+        <Button>Submit</Button>
+      </form>
     </Grid>
-   
+
   )
 }
- export default JobForm
+export default JobForm
