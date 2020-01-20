2d26671a4154cec178db1ba16f4adc947cce6b5a
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-01-20 15:45:39 -0800

    User Auth

---
+++ routes/userRoutes.js
@@ -0,0 +1,30 @@
+const { User } = require ('../models')
+const jwt = require('jsonwebtoken')
+
+module.exports = app => {
+    //Register a User
+    app.post('/users', (req, res) =>{
+        const { name, email, username } = req.body
+        User.register(new User({ name, email, username }),req.body.password, e =>{
+                if(e) {
+                    console.log(e)
+                }
+                res.sendStatus(200)
+            })
+    })  
+
+    // Login 
+    app.post('/login', (req,res) => {
+        User.authenticate()(req.body.username, req.body.password, (e, user) => {
+            if(e) {
+                console.log(e)
+            }
+            if (user) {
+                res.json({token: jwt.sign({ id: user._id}, process.env.SECRET)
+            })
+            }else {
+                res.sendStatus(404)
+            }
+      })
+    })
+}
--- server.js
+++ server.js
@@ -3,11 +3,21 @@ require('dotenv').config()
 const express = require('express')
 const { join } = require('path')
 
+const passport = require('passport')
+const { Strategy } = require('passport-local')
 const app = express()
+const { User } = require('./models')
 
 app.use(express.static(join(__dirname, 'client', 'build' )))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(new Strategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 require('./routes')(app)
 
