ae493a4a0e3f2ceffdf7881177434ffd5978adb2
Sabrina <delrock876@gmail.com>
2020-01-20 18:44:25 -0800

    added placeholder for jobcard

--- client/src/components/JobCard/JobCard.js
+++ client/src/components/JobCard/JobCard.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import Chip from '@material-ui/core/Chip'
 
 const useStyles = makeStyles({
   card: {
@@ -21,33 +22,49 @@ const useStyles = makeStyles({
   pos: {
     marginBottom: 12,
   },
-});
+})
 
-export default function OutlinedCard() {
-  const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
+const JobCard =()=> {
+  const classes = useStyles()
+  const bull = <span className={classes.bullet}>•</span>
 
   return (
     <Card className={classes.card} variant="outlined">
       <CardContent>
         <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
+          [DATE APPLIED]
         </Typography>
         <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
+          [FAKE COMPANY NAME]
         </Typography>
         <Typography className={classes.pos} color="textSecondary">
-          adjective
+          [JOBTITLE]
         </Typography>
+
+
         <Typography variant="body2" component="p">
-          well meaning and kindly.
+          Job Requirements: 
           <br />
-          {'"a benevolent smile"'}
+          <Chip color= "primary" label="Javascript" variant="outlined" />
+          <Chip color="primary" label="MongoDb" variant="outlined" />
         </Typography>
+      
+        <br/>
+
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          [CONNECTION POSITION]: [CONNECTION NAME]
+          <br/>
+          [CONNECTION PHONE]
+          <br />
+          [CONNECTION EMAIL]
+          <br />
+        </Typography>
+      
       </CardContent>
       <CardActions>
         <Button size="small">Learn More</Button>
       </CardActions>
     </Card>
-  );
+  )
 }
+ export default JobCard
