b1eed2359ecacbd8768c748b188c8a7b14c47bdd
E L A <ela@dhcp-v077-012.mobile.uci.edu>
2020-01-23 19:37:44 -0800

    removed Calendar component

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import SimCard from '../../components/Card'
 // import Navbar from '../../components/Navbar'
-import Calendar from '../../components/Calendar'
+
 
 const Home = () => {
 
