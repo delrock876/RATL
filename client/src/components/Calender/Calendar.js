d5dc1a8b77469823253d0d4a39df61bad1acaa66
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-01-28 18:08:47 -0800

    Modal component

--- client/src/components/Calender/Calendar.js
+++ client/src/components/Calender/Calendar.js
@@ -1,12 +1,14 @@
-import React,{ Component } from 'react'
+
+import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction' // needed for dayClick
+import '../Modal/Modal'
+
 import './Calendar.scss'
 
-
-class Calendar extends Component {
+export default class DemoApp extends React.Component {
 
   calendarComponentRef = React.createRef()
   state = {
@@ -16,6 +18,24 @@ class Calendar extends Component {
     ]
   }
 
+
+  toggleWeekends = () => {
+    this.setState({ // update a property
+      calendarWeekends: !this.state.calendarWeekends
+    })
+  }
+
+  gotoPast = () => {
+    let calendarApi = this.calendarComponentRef.current.getApi()
+    calendarApi.gotoDate('2000-01-01') // call a method on the Calendar object
+  }
+
+  handleDateClick = (event) => {
+   console.log('hi')
+  }
+
+  
+  
   render() {
     return (
       <div className='demo-app'>
@@ -38,33 +58,11 @@ class Calendar extends Component {
             events={ this.state.calendarEvents }
             dateClick={ this.handleDateClick }
             />
+       
         </div>
       </div>
     )
   }
 
-  toggleWeekends = () => {
-    this.setState({ // update a property
-      calendarWeekends: !this.state.calendarWeekends
-    })
-  }
-
-  gotoPast = () => {
-    let calendarApi = this.calendarComponentRef.current.getApi()
-    calendarApi.gotoDate('2000-01-01') // call a method on the Calendar object
-  }
-
-  handleDateClick = (arg) => {
-    if (window.confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
-      this.setState({  // add new event data
-        calendarEvents: this.state.calendarEvents.concat({ // creates a new array
-          title: 'Add Some Event',
-          start: arg.date,
-          allDay: arg.allDay
-        })
-      })
-    }
-  }
 
 }
-export default Calendar
