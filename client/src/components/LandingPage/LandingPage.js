8307118220db508683a19846747816b939f5ea09
E L A <ela@dhcp-v082-016.mobile.uci.edu>
2020-01-28 20:09:27 -0800

    updated styling to landing page

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -11,22 +11,12 @@ import LocationCityIcon from '@material-ui/icons/LocationCity'
 import SearchIcon from '@material-ui/icons/Search'
 
 
-class MyHeader extends React.Component {
-  render() {
-    const mystyle = {
-      color: "DB9A0D",
-      backgroundColor: "FFFFFF",
-      padding: "10px",
-    };
-    return (
-      <div>
-        <h3 style={mystyle}>Your efficient job search management tool</h3>
-        <p> RATL effectively helps manage your job search by providing tools to keep track of your application process, stay in touch with hiring managers and never loose a beat with an overview of key dates on your calendar such as upcoming interviews and networking events.  </p>
-      </div>
-    );
-  }
-}
 const useStyles = makeStyles(theme => ({
+  header: {
+    color: "DB9A0D",
+    backgroundColor: "FFFFFF",
+    padding: "10px",
+  },
   root: {
     flexGrow: 1,
   },
@@ -41,40 +31,60 @@ const LandingPage = () => {
   const classes = useStyles();
   return (
     <>
-    <div>
-      <Parallax
-        blur={0}
-        bgImage={require('./assets/laptop-1209008_1280.jpg')}
-        bgImageAlt="group"
-        strength={200}
-      >
-        <div style={{ height: '250px' }} />
-      </Parallax>
-    </div>
+      <div>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Header className={classes.header}>Your efficient job search management tool</h3>
+            <p> RATL effectively helps manage your job search by providing tools to keep track of your application process, stay in touch with hiring managers and never loose a beat with an overview of key dates on your calendar such as upcoming interviews and networking events.  </p>
+          </Grid>
+        </Grid>
+      </div>
 
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}> <h4> Manage Connections </h4> <PermContactCalendarIcon />
-            <p> Have key contact information readily on-hand so communication with hiring managers can be easily maintained</p></Paper>
-        </Grid>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}> <h4> Calendar View </h4>  <TodayIcon />
-            <p> Stay ahead of the game - keep track of important dates like upcoming interviews,  follow-up calls and networking events </p></Paper>
-        </Grid>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>  <h4> Job Leads </h4>   <LocationCityIcon />
-            <p> Receive an overview of latest listings in your custom feed </p></Paper>
-        </Grid>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>  <h4> Company Insights </h4> <SearchIcon />
-            <p> Be in-the-know and armed with relevant information about companies & organizations  </p></Paper>
-      </Grid>
+      <div>
+        <Parallax
+          blur={0}
+          bgImage={require('./assets/Group-of-People.jpg')}
+          bgImageAlt="group"
+          strength={200}
+        >
+          <div style={{ height: '450px' }} />
+        </Parallax>
+      </div>
 
-      </Grid>
-    </div >
-      </>
-)
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}> <h4> Manage Connections </h4> <PermContactCalendarIcon />
+              <p> Have key contact information readily on-hand so communication with hiring managers can be easily maintained</p></Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}> <h4> Calendar View </h4>  <TodayIcon />
+              <p> Stay ahead of the game - keep track of important dates like upcoming interviews,  follow-up calls and networking events </p></Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>  <h4> Job Leads </h4>   <LocationCityIcon />
+              <p> Receive an overview of latest listings in your custom feed </p></Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>  <h4> Company Insights </h4> <SearchIcon />
+              <p> Be in-the-know and armed with relevant information about companies & organizations  </p></Paper>
+          </Grid>
+
+        </Grid>
+      </div >
+
+      <div>
+        <Parallax
+          blur={0}
+          bgImage={require('./assets/female_job_hunt.jpg')}
+          bgImageAlt="group"
+          strength={200}
+        >
+          <div style={{ height: '400px' }} />
+        </Parallax>
+      </div>
+    </>
+  )
 }
 
 
