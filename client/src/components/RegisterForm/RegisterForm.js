e8d93085a10e556705bd8e4df5991fbdf555c783
Sabrina <delrock876@gmail.com>
2020-01-29 01:22:32 -0800

    improved, working modal

--- client/src/components/RegisterForm/RegisterForm.js
+++ client/src/components/RegisterForm/RegisterForm.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import UserContext from '../../utils/UserContext'
-import swal from '@sweetalert/with-react'
 import { useContext } from 'react'
+
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Modal from 'react-bootstrap/Modal'
+
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -13,113 +20,138 @@ const useStyles = makeStyles(theme => ({
       width: 200,
     },
   },
+  card: {
+    maxWidth: 490,
+  },
+  pos: {
+    margin: 10
+  }
 }))
 
-const RegisterForm = () => {
-  const {userFullName, usersname, userEmail, userPassword, handleInputChange, handleRegisterUser} = useContext(UserContext)
-  // const [value, setValue] = React.useState('Controlled');
+const RegisterForm =()=> {
+  const { userFullName, usersname, userEmail, userPassword, handleInputChange, handleRegisterUser } = useContext(UserContext)
+
   const classes = useStyles()
-let form
-  function clickMe (){
-    swal (form)
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const closeAndRegister =()=>{
+    handleClose()
+    handleRegisterUser()
   }
 
   return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Register
+      </Button>
 
-    <Button onClick= {clickMe => swal(
-      <form className={classes.root} noValidate autoComplete="off">
-
-        <div>
-          <TextField
-            id="outlined-multiline-flexible"
-            label="Full Name"
-            name="userFullName"
+      <Modal show={show} onHide={handleClose} animation={true}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create an Account!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form className={classes.card}>
+            <TextField 
+            className={classes.pos}
+            name= "userFullName"
             value={userFullName}
-            placeholder="Create a username"
-            onChange={handleInputChange}
-            variant="outlined"
-          />
-          <TextField
-            id="outlined-multiline-flexible"
-            label="Username"
-            name="usersname"
-            value={usersname}
-            placeholder="Create a username"
-            onChange={handleInputChange}
-            variant="outlined"
-          />
-          <TextField
-            id="outlined-multiline-flexible"
-            label="Email"
+            label= "Your Name" 
+            variant="outlined"></TextField>
+
+            <TextField 
+            className={classes.pos}
             name="userEmail"
             value={userEmail}
-            placeholder="johndoe@email.com"
-            onChange={handleInputChange}
-            variant="outlined"
-          />
-          <TextField
-            id="outlined-multiline-flexible"
-            label="Password"
-            name="userPassword"
-            value={userPassword}
-            placeholder="Create a password"
-            onChange={handleInputChange}
-            variant="outlined"
-          />
+            label= "Email" 
+            variant="outlined"></TextField>
 
-          <Button onClick={handleRegisterUser} variant="contained" color="secondary">Register!</Button>
+            <TextField 
+            className={classes.pos}
+            name="usersname"
+            value= {usersname}
+            label= "Username" 
+            variant="outlined"></TextField>
 
-        </div>
-      </form>
-    )}>Register</Button>
-  //   swal(
-  //   <form className={classes.root} noValidate autoComplete="off">
-      
-  //     <div>
-  //       <TextField
-  //         id="outlined-multiline-flexible"
-  //         label="Full Name"
-  //         name="userFullName"
-  //         value={userFullName}
-  //         placeholder= "Create a username"
-  //         onChange={handleInputChange}
-  //         variant="outlined"
-  //       />
-  //       <TextField
-  //         id="outlined-multiline-flexible"
-  //         label="Username"
-  //         name= "usersname"
-  //         value={usersname}
-  //         placeholder= "Create a username"
-  //         onChange={handleInputChange}
-  //         variant="outlined"
-  //       />
-  //       <TextField
-  //         id="outlined-multiline-flexible"
-  //         label="Email"
-  //         name= "userEmail"
-  //         value={userEmail}
-  //         placeholder= "johndoe@email.com"
-  //         onChange={handleInputChange}
-  //         variant="outlined"
-  //       />
-  //       <TextField
-  //         id="outlined-multiline-flexible"
-  //         label="Password"
-  //         name= "userPassword"
-  //         value={userPassword}
-  //         placeholder= "Create a password"
-  //         onChange={handleInputChange}
-  //         variant="outlined"
-  //       />
-        
-  //       <Button onClick = {handleRegisterUser} variant= "contained" color= "secondary">Register!</Button>
-       
-  //     </div>
-  //   </form>
-  // )
-  // // end swal
-  )
+            <TextField 
+            className={classes.pos}
+            name= "userPassword"
+            value= {userPassword}
+            label= "Password" 
+            placeholder="Create a password!" 
+            variant="outlined"></TextField>
+          </form>
+          
+          </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={closeAndRegister}>
+            Register Account!
+          </Button>
+          
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
 
 export default RegisterForm
+
+
+// const RegisterForm = () => {
+//   const {userFullName, usersname, userEmail, userPassword, handleInputChange, handleRegisterUser} = useContext(UserContext)
+//   // const [value, setValue] = React.useState('Controlled');
+//   const classes = useStyles()
+
+//   return (
+//     <Card className= {classes.card}>
+//       <CardContent className= {classes.pos}>
+//     <form className={classes.root} noValidate autoComplete="off" >
+      
+//         <TextField
+//           id="outlined-multiline-flexible"
+//           label="Full Name"
+//           name="userFullName"
+//           value={userFullName}
+//           placeholder= "Create a username"
+//           onChange={handleInputChange}
+//           variant="outlined"
+//         />
+//         <TextField
+//           id="outlined-multiline-flexible"
+//           label="Username"
+//           name= "usersname"
+//           value={usersname}
+//           placeholder= "Create a username"
+//           onChange={handleInputChange}
+//           variant="outlined"
+//         />
+//         <TextField
+//           id="outlined-multiline-flexible"
+//           label="Email"
+//           name= "userEmail"
+//           value={userEmail}
+//           placeholder= "johndoe@email.com"
+//           onChange={handleInputChange}
+//           variant="outlined"
+//         />
+//         <TextField
+//           id="outlined-multiline-flexible"
+//           label="Password"
+//           name= "userPassword"
+//           value={userPassword}
+//           placeholder= "Create a password"
+//           onChange={handleInputChange}
+//           variant="outlined"
+//         />
+//         <br/>
+//         <Button onClick = {handleRegisterUser} variant= "contained" color= "secondary">Register!</Button>
+       
+//     </form>
+//     </CardContent>
+//     </Card>
+//   )
+// }
+
+// export default RegisterForm
