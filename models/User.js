9a7e4093f166a604483666b5714297dc61abba14
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-01-30 19:38:51 -0800

    populate calendar to user

--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ module.exports = (model, Schema) => {
         name: String,
         email: String,
         username: String,
-        userJobs: [{ type: Schema.Types.ObjectId, ref: 'Jobs' }]
+        userJobs: [{ type: Schema.Types.ObjectId, ref: 'Jobs' }],
+        calendar: [{type: Schema.Types.ObjectId, ref: 'Calendar'}]
     })
 
     User.plugin(require('passport-local-mongoose'))
