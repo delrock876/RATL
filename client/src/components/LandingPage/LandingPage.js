dcc0b9dbd727d9f574a20adc2ad7b4b4af6e9f0f
E L A <ela@dhcp-v091-195.mobile.uci.edu>
2020-02-01 11:45:26 -0800

    updated landing page style + assets

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -9,14 +9,18 @@ import PermContactCalendarIcon from '@material-ui/icons/PermContactCalendar'
 import TodayIcon from '@material-ui/icons/Today'
 import LocationCityIcon from '@material-ui/icons/LocationCity'
 import WorkIcon from '@material-ui/icons/Work'
-import snakeImage from './assets/Snake-Vector-Illustration.jpg'
+// import logo from './assets/RATL.png'
 
 
 
 const useStyles = makeStyles(theme => ({
   header: {
-    color: "DB9A0D",
     backgroundColor: "FFFFFF",
+    padding: "10px",
+    textAlign: "center",
+  },
+
+  logo: {
     padding: "10px",
     textAlign: "center",
   },
@@ -37,7 +41,7 @@ const LandingPage = () => {
       <div>
         <Grid container spacing={3}>
           <Grid item xs={12}>
-            <Paper className={classes.header}><h3>Your efficient job search management tool</h3>
+            <Paper elevation={0} className={classes.header} image="./assets/RATL.png"><h3>Your efficient job search management tool</h3>
               <p> RATL effectively helps manage your job search by providing tools to keep track of your application process, stay in touch with hiring managers and never loose a beat with an overview of key dates on your calendar such as upcoming interviews and networking events.  </p>
             </Paper>
           </Grid>
@@ -58,20 +62,20 @@ const LandingPage = () => {
 
       <div className={classes.root}>
         <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}> <h4> Manage Connections </h4> <PermContactCalendarIcon />
+          <Grid item xs={6}>
+            <Paper elevation={0} className={classes.paper}> <h4> Manage Connections </h4> <PermContactCalendarIcon />
               <p> Have key contact information readily on-hand so communication with hiring managers can be easily maintained</p></Paper>
           </Grid>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}> <h4> Calendar View </h4>  <TodayIcon />
+          <Grid item xs={6}>
+            <Paper elevation={0} className={classes.paper}> <h4> Calendar View </h4>  <TodayIcon />
               <p> Stay ahead of the game - keep track of important dates like upcoming interviews,  follow-up calls and networking events </p></Paper>
           </Grid>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}>  <h4> Job Leads </h4>   <WorkIcon />
+          <Grid item xs={6}>
+            <Paper elevation={0} className={classes.paper}>  <h4> Job Leads </h4>   <WorkIcon />
               <p> Receive an overview of latest listings in your custom feed </p></Paper>
           </Grid>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}>  <h4> Company Insights </h4> <LocationCityIcon />
+          <Grid item xs={6}>
+            <Paper elevation={0} className={classes.paper}>  <h4> Company Insights </h4> <LocationCityIcon />
               <p> Be in-the-know and armed with relevant information about companies & organizations  </p></Paper>
           </Grid>
 
@@ -80,9 +84,9 @@ const LandingPage = () => {
       <div>
         <Parallax
           blur={0}
-          bgImage={require('./assets/Group-of-People.jpg')}
+          bgImage={require('./assets/female-looking-at-laptop.jpg')}
           bgImageAlt="group"
-          strength={200}
+          strength={400}
         >
           <div style={{ height: '450px' }} />
         </Parallax>
