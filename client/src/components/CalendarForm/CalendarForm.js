17ceffc99470cb4fab5afbf3a8983c85e825da78
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-02-07 16:58:59 -0800

    Calendar Form

---
+++ client/src/components/CalendarForm/CalendarForm.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+const {getAllReminders} = (Calendar)
+
+
+const CalendarForm = () => {
+
+    return( 
+
+    <Card className='Form'>
+        <CardContent>
+        
+        </CardContent>
+
+    </Card>
+    )
+
+}
+
+export default CalendarForm
